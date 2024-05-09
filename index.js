19321647fd6d1f806d33d5fcf41d2bad0c91e6ab
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2024-05-09 15:49:38 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	
+	socket.on("chat message", (msg) => {
+		console.log("message: " + msg);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+		
+});
+
+httpServer.listen(3000, () => {
+	console.log("listening on *:3000");
+});
